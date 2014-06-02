d56d8d0f8b51a3542ba2b15506163289172fdf1e
Erick <erick.peirson@asu.edu>
2014-06-02 15:56:18 -0700

    docs

---
+++ doc/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,organ:0,modul:0,indic:0,content:0,tabl:0,neurosci:0,model:0,document:0,page:0},objtypes:{},titles:["Welcome to Model Organisms in Neuroscience&#8217;s documentation!"],objnames:{},filenames:["index"]})
